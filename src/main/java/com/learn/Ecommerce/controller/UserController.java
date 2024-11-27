92e717cff126f66616f67dbfe5cc3614579f5cd8
Priya-Vibhute <priyanka.vibhute@itvedant.com>
2024-11-27 07:20:07 +0530

    27-11-2024

---
+++ src/main/java/com/learn/Ecommerce/controller/UserController.java
@@ -0,0 +1,74 @@
+package com.learn.Ecommerce.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learn.Ecommerce.Dto.UserDto;
+import com.learn.Ecommerce.service.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@Autowired
+	UserService userService;
+	
+//	POST  ["/users"] To add user
+	@PostMapping
+	public ResponseEntity<UserDto> addUser(@RequestBody @Valid UserDto userDto)
+	{
+		UserDto savedUser = userService.addUser(userDto);
+		
+		 return  new ResponseEntity<UserDto>(savedUser,HttpStatus.CREATED) ;
+	}
+
+//GET ["/users/101}"] To fetch user by id
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDto> getUserById(@PathVariable String id)
+	{
+		UserDto userDto = userService.getUserById(id);
+		return new ResponseEntity<UserDto>(userDto,HttpStatus.FOUND);
+	}
+	
+//GET ["/users"] To fetch all users
+	@GetMapping
+	public ResponseEntity<List<UserDto>> fetchAllusers()
+	{
+		List<UserDto> users = userService.getAllUsers();
+		return new ResponseEntity<List<UserDto>>(users,HttpStatus.OK);
+	}
+	
+//DELETE ["/users/{id}"]
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteUser(@PathVariable String id)
+	{
+		String message = userService.deleteUser(id);
+		return new ResponseEntity<String>(message,HttpStatus.OK);
+	}
+// PUT {"/users/{id}"}
+	@PutMapping("/{id}")
+	public ResponseEntity<UserDto> updateUser(@PathVariable String id, 
+			           @RequestBody UserDto userDto)
+	{
+		UserDto updateUser = userService.updateUser(id,userDto);
+		
+		return new ResponseEntity<UserDto>(updateUser,HttpStatus.OK);
+	}
+	
+	
+}
---
+++ src/main/java/com/learn/Ecommerce/service/UserServiceImpl.java
@@ -0,0 +1,105 @@
+package com.learn.Ecommerce.service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.learn.Ecommerce.Dto.UserDto;
+import com.learn.Ecommerce.entity.User;
+import com.learn.Ecommerce.repository.UserRepository;
+
+@Service
+public class UserServiceImpl  implements UserService {
+   
+	@Autowired
+	UserRepository userRepository;
+    
+	@Override
+	public UserDto addUser(UserDto userDto) {
+		UUID randomUUID = UUID.randomUUID();
+		String id = randomUUID.toString();
+		userDto.setId(id);
+		
+		User user = dtoToEntity(userDto);
+		User savedUser = userRepository.save(user);
+		
+		UserDto savedDto = entityToDto(savedUser);
+		return savedDto;
+	}
+
+	@Override
+	public List<UserDto> getAllUsers() {
+		
+		List<User> users = userRepository.findAll();
+		
+		List<UserDto> userDtoList = users.stream()
+		.map(u->entityToDto(u))
+		.collect(Collectors.toList());
+		
+		return userDtoList;
+	}
+
+	@Override
+	public UserDto getUserById(String id) {
+	  
+		User user = userRepository.findById(id)
+	    .orElseThrow(()->new RuntimeException(id+" not found"));
+	    
+		return entityToDto(user);
+	}
+
+	@Override
+	public UserDto updateUser(String id, UserDto userDto) {
+		
+		User user = userRepository.findById(id)
+		.orElseThrow(()->new RuntimeException(id+" not found"));
+		
+		user.setFirstName(userDto.getFirstName());
+		user.setLastName(userDto.getLastName());
+		user.setEmailId(userDto.getEmailId());
+		user.setAge(userDto.getAge());
+		user.setPassword(userDto.getPassword());
+		
+		User updateUser = userRepository.save(user);
+		
+		return entityToDto(updateUser);
+	}
+
+	@Override
+	public String deleteUser(String id) {
+	    User user = userRepository.findById(id)
+	    .orElseThrow(()->new RuntimeException(id+ " not found"));
+	    
+	    userRepository.delete(user);
+		return user.getId()+"deleted successfully";
+	}
+
+	@Override
+	public UserDto entityToDto(User user) {
+		UserDto userDto = new UserDto();
+		userDto.setId(user.getId());
+		userDto.setFirstName(user.getFirstName());
+		userDto.setLastName(user.getLastName());
+		userDto.setPassword(user.getPassword());
+		userDto.setEmailId(user.getEmailId());
+		userDto.setAge(user.getAge());
+		
+		return userDto;
+	}
+
+	@Override
+	public User dtoToEntity(UserDto userDto) {
+		User user=new User();
+		user.setId(userDto.getId());
+		user.setFirstName(userDto.getFirstName());
+		user.setLastName(userDto.getLastName());
+		user.setPassword(userDto.getPassword());
+		user.setAge(userDto.getAge());
+		user.setEmailId(userDto.getEmailId());
+		return user;
+	}
+
+}
